6a3a71d8e46e253fbe2ab6ec6e84e5e17e016366
Jim Holmberg <hasenpfad1@gmail.com>
2019-04-01 18:32:48 -0500

    Just making sure the gifs start and stop

--- assets/javascript/GIPHYButtons.js
+++ assets/javascript/GIPHYButtons.js
@@ -10,45 +10,28 @@ var playerWins = 0;
 
 $(document).on("click", ".rockStar", function() {
 	$("#rockerGIFS").empty();
-	//$("button").on("click", function() {
-	// In this case, the "this" keyword refers to the button that was clicked
 	var person = $(this).attr("data-name");
 	console.log(person);
-
-	// Constructing a URL to search Giphy for the name of the person who said the quote
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
 	  person + "&api_key=G8bzbqfK3z3GbTVircARgYTm2R9lLOjL&limit=10";
 
-	// Performing our AJAX GET request
 	$.ajax({
 	  url: queryURL,
 	  method: "GET"
 	})
-	  // After the data comes back from the API
+
 	  .then(function(response) {
 		$("#rockerGIFS").empty();
-		// Storing an array of results in the results variable
+		//note results is an array
 		var results = response.data;
-
-		// Looping over every result item
 		for (var i = 0; i < results.length; i++) {
-
-		  // Only taking action if the photo has an appropriate rating
 		  if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 			// Creating a div for the gif
 			var gifDiv = $("<div>");
 
 			gifDiv.attr("class", "rockerGIFbutton");
-
-			//gifDiv.attr("data-state", "still");
-
-			// Storing the result item's rating
 			var rating = results[i].rating;
-
-			// Creating a paragraph tag with the result item's rating
 			var p = $("<p>").text("Rating: " + rating);
-
-			// Creating an image tag
 			var personImage = $("<img>");
 
 			// Giving the image tag an src attribute of a proprty pulled off the
@@ -56,13 +39,10 @@ $(document).on("click", ".rockStar", function() {
 			personImage.attr("src", results[i].images.fixed_height_still.url);
 			personImage.attr("data-still", results[i].images.fixed_height_still.url);
 			personImage.attr("data-animate", results[i].images.fixed_height.url);
-			//personImage.attr("data-state", "still");
 			personImage.attr("class", "rockStarImage");
 			// Appending the paragraph and personImage we created to the "gifDiv" div we created
 			gifDiv.append(p);
 			gifDiv.append(personImage);
-
-			// Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
 			$("#rockerGIFS").prepend(gifDiv);
 		  }
 		}
@@ -85,20 +65,15 @@ $(document).on("click", ".rockStar", function() {
 	}
   });
 function rockerButtonLoads(){
-
-	// Deleting the movie buttons prior to adding new movie buttons
-	// (this is necessary otherwise we will have repeat buttons)
 	$("#rockerButtons").empty();
 
 	// Looping through the array of movies
 	for (var i = 0; i < rockerButtonsArray.length; i++) {
 
-	  // Then dynamicaly generating buttons for each movie in the array.
+	  // Then dynamicaly generating buttons for each rockStar in the array.
 	  // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
 	  var a = $("<button>");
-	  // Adding a class
 	  a.addClass("rockStar");
-	  // Adding a data-attribute with a value of the movie at index i
 	  a.attr("data-name", rockerButtonsArray[i]);
 	  // Providing the button's text with a value of the movie at index i
 	  a.text(rockerButtonsArray[i]);
